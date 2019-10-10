1bca1e310c6330792ab034bee4166d9db8352a5b
ImaldiEgyS <55644926+ImaldiEgyS@users.noreply.github.com>
2019-10-10 23:43:31 +0700

    Add files via upload

---
+++ phpmvc/app/controllers/about.php
@@ -0,0 +1,24 @@
+<?php
+class about extends Controller
+{
+    public function index($nama = 'Egy Imaldi Sulistyo', $pekerjaan= 'Mahasiswa', $umur = 19){
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About me';
+        
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+    }
+    
+    public function page()
+    {
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+    }
+
+    
+}
